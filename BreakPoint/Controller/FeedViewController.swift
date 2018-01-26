e46ed79311224afebcb39eb807cd853ea6bb39c4
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-26 17:30:56 +0100

    Link data to table view cells

--- BreakPoint/Controller/FeedViewController.swift
+++ BreakPoint/Controller/FeedViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    //MARK: - Outlets
+    @IBOutlet weak var tableView: UITableView!
+
+    //MARK: - Variables
+    var messages = [Message]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        DataService.instance.getAllFeedMessages { (messages) in
+            self.messages = messages
+            self.tableView.reloadData()
+        }
     }
 
+    //MARK: - UITableViewDelegate and UITableViewDataSource
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "feedCell") as? FeedTableViewCell
+            else { return UITableViewCell() }
+        cell.configure(profileImage: #imageLiteral(resourceName: "defaultProfileImage"), email: messages[indexPath.row].senderId, content: messages[indexPath.row].content)
+        return cell
+    }
 }
 
