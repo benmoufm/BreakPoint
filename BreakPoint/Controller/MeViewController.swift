6d06e12a9b79d66bb0e42ea7e325b3410556ecef
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-26 16:04:41 +0100

    Create me view controller and connect outlets and actions

---
+++ BreakPoint/Controller/MeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MeViewController.swift
+//  BreakPoint
+//
+//  Created by Mélodie Benmouffek on 26/01/2018.
+//  Copyright © 2018 Mélodie Benmouffek. All rights reserved.
+//
+
+import UIKit
+
+class MeViewController: UIViewController {
+    //MARK: - Outlets
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    //MARK: - Actions
+    @IBAction func signoutButtonPressed(_ sender: Any) {
+
+    }
+}
