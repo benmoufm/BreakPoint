f7ecb86453cdce63e6322a9dc01b8c852472e72f
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-29 17:12:59 +0100

    Create group feed view controller and connect outlets and actions

---
+++ BreakPoint/Controller/GroupFeedViewController.swift
@@ -0,0 +1,32 @@
+//
+//  GroupFeedViewController.swift
+//  BreakPoint
+//
+//  Created by Mélodie Benmouffek on 29/01/2018.
+//  Copyright © 2018 Mélodie Benmouffek. All rights reserved.
+//
+
+import UIKit
+
+class GroupFeedViewController: UIViewController {
+    //MARK: - Outlets
+    @IBOutlet weak var groupTitleLabel: UILabel!
+    @IBOutlet weak var membersLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var sendMessageView: UIView!
+    @IBOutlet weak var messageTextField: InsetTextField!
+    @IBOutlet weak var sendButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    //MARK: - Actions
+    @IBAction func backButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func sendButtonPressed(_ sender: Any) {
+
+    }
+}
