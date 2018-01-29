9cff0ab9928778dc846c0f8d1335936f8c5e7d59
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-29 12:25:32 +0100

    Conform groups view controller to tableview delegate and datasource

--- BreakPoint/Controller/GroupsViewController.swift
+++ BreakPoint/Controller/GroupsViewController.swift
@@ -8,18 +8,30 @@
 
 import UIKit
 
-class GroupsViewController: UIViewController {
+class GroupsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    //MARK: - Outlets
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: - UITableViewDelegate & UITableViewDataSource
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "groupCell") as? GroupTableViewCell
+            else { return UITableViewCell() }
+        cell.configureCell(title: "Gotham", description: "The group Gotham deserves", memberCount: 3)
+        return cell
+    }
 }
 
