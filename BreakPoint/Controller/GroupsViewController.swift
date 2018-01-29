cf8a08e3abbeab9d023f7cdb6ac5c38ced22e951
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-29 16:58:16 +0100

    Get groups from firebase and show in tableview

--- BreakPoint/Controller/GroupsViewController.swift
+++ BreakPoint/Controller/GroupsViewController.swift
@@ -12,10 +12,23 @@ class GroupsViewController: UIViewController, UITableViewDataSource, UITableView
     //MARK: - Outlets
     @IBOutlet weak var tableView: UITableView!
 
+    //MARK: - Variables
+    var groups = [Group]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        DataService.instance.REF_GROUPS.observe(.value) { (snapshot) in
+            DataService.instance.getAllGroups { (groups) in
+                self.groups = groups
+                self.tableView.reloadData()
+            }
+        }
     }
 
     //MARK: - UITableViewDelegate & UITableViewDataSource
@@ -24,13 +37,15 @@ class GroupsViewController: UIViewController, UITableViewDataSource, UITableView
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return groups.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "groupCell") as? GroupTableViewCell
             else { return UITableViewCell() }
-        cell.configureCell(title: "Gotham", description: "The group Gotham deserves", memberCount: 3)
+        cell.configureCell(title: groups[indexPath.row].title,
+                           description: groups[indexPath.row].description,
+                           memberCount: groups[indexPath.row].memberCount)
         return cell
     }
 }
