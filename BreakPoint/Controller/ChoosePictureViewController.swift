e22b7248bd604fa55f8aff2cf8440dc887390f3e
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-02-06 11:45:37 +0100

    Add segmented control for choose picture view controller

--- BreakPoint/Controller/ChoosePictureViewController.swift
+++ BreakPoint/Controller/ChoosePictureViewController.swift
@@ -14,6 +14,7 @@ class ChoosePictureViewController: UIViewController {
     var closeButton = UIButton()
     var titleLabel = UILabel()
     var doneButton = UIButton()
+    var segmentedControl = UISegmentedControl()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +31,7 @@ class ChoosePictureViewController: UIViewController {
         navigationView.addSubview(doneButton)
         navigationView.addSubview(titleLabel)
         navigationView.addSubview(closeButton)
+        view.addSubview(segmentedControl)
 
         navigationView.translatesAutoresizingMaskIntoConstraints = false
         navigationView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
@@ -52,6 +54,12 @@ class ChoosePictureViewController: UIViewController {
         closeButton.centerYAnchor.constraint(equalTo: doneButton.centerYAnchor).isActive = true
         closeButton.heightAnchor.constraint(equalToConstant: 20.0).isActive = true
         closeButton.widthAnchor.constraint(equalToConstant: 20.0).isActive = true
+
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        segmentedControl.topAnchor.constraint(equalTo: navigationView.bottomAnchor, constant: 20.0).isActive = true
+        segmentedControl.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        segmentedControl.widthAnchor.constraint(equalToConstant: 200.0).isActive = true
+        segmentedControl.heightAnchor.constraint(equalToConstant: 30.0).isActive = true
     }
 
     private func setupNavigationView() {
@@ -74,12 +82,20 @@ class ChoosePictureViewController: UIViewController {
         doneButton.setTitleColor(#colorLiteral(red: 0.6212110519, green: 0.8334299922, blue: 0.3770503998, alpha: 1), for: .normal)
     }
 
+    private func setupSegmentedControl() {
+        segmentedControl.insertSegment(withTitle: "Dark", at: 0, animated: true)
+        segmentedControl.insertSegment(withTitle: "Light", at: 1, animated: true)
+        segmentedControl.tintColor = #colorLiteral(red: 0, green: 0.7235742211, blue: 0.8151144385, alpha: 1)
+        segmentedControl.selectedSegmentIndex = 0
+    }
+
     private func setup() {
         view.backgroundColor = #colorLiteral(red: 0.2126879096, green: 0.2239724994, blue: 0.265286684, alpha: 1)
         setupNavigationView()
         setupCloseButton()
         setupTitleLabel()
         setupDoneButton()
+        setupSegmentedControl()
         setupLayout()
     }
 }
