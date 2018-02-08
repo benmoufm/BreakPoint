b8917e9a6b304866fee3847b92ae338827cd5144
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-02-08 14:22:19 +0100

    Add upload and download functions to storage service

--- BreakPoint/Services/StorageService.swift
+++ BreakPoint/Services/StorageService.swift
@@ -9,10 +9,30 @@
 import Foundation
 import Firebase
 
-let STORAGE_BASE = Storage.storage().reference()
+let STORAGE_BASE = Storage.storage()
 
 class StorageService {
     static let instance = StorageService()
 
-    private(set) var REF_IMAGES = STORAGE_BASE.child("images")
+    private(set) var REF_IMAGES = STORAGE_BASE.reference().child("images")
+
+    func uploadPicture(image: UIImage, uid: String, completion: @escaping (_ url: String) -> Void) {
+        var data = Data()
+        data = UIImagePNGRepresentation(image)!
+        let imageRef = REF_IMAGES.child(uid)
+        _ = imageRef.putData(data, metadata: nil, completion: { (metadata, error) in
+            guard let metaData = metadata else { return }
+            let downloadURL = metaData.downloadURL()
+            completion((downloadURL?.absoluteString)!)
+        })
+    }
+
+    func downloadPicture(url: String, completion: @escaping (UIImage) -> Void) {
+        let httpsRef = STORAGE_BASE.reference(forURL: url)
+        httpsRef.getData(maxSize: 1 * 1024 * 1024) { (data, error) in
+            guard let data = data else { return }
+            let image = UIImage(data: data)!
+            completion(image)
+        }
+    }
 }
